d0590e6a24a3e7cd7f9d5dd47fbb3579aa508d33
unaganteng <rismasykes10@gmail.com>
2021-06-28 21:38:18 +0700

    upload

---
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -0,0 +1,223 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('admin')}}">
+      <div class="sidebar-brand-icon rotate-n-15">
+        <i class="fas fa-laugh-wink"></i>
+      </div>
+      <div class="sidebar-brand-text mx-3">Admin</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+      <a class="nav-link" href="{{route('admin')}}">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Banner
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('file-manager')}}">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Manajemen Media</span></a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-image"></i>
+        <span>Banners</span>
+      </a>
+      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Pilihan Banners:</h6>
+          <a class="collapse-item" href="{{route('banner.index')}}">Banner</a>
+          <a class="collapse-item" href="{{route('banner.create')}}">Tambah Banner</a>
+        </div>
+      </div>
+    </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Shop
+        </div>
+
+    <!-- Categories -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#categoryCollapse" aria-expanded="true" aria-controls="categoryCollapse">
+          <i class="fas fa-sitemap"></i>
+          <span>Kategori</span>
+        </a>
+        <div id="categoryCollapse" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Pilihan Kategori:</h6>
+            <a class="collapse-item" href="{{route('category.index')}}">Kategori</a>
+            <a class="collapse-item" href="{{route('category.create')}}">Tambah Kategori</a>
+          </div>
+        </div>
+    </li>
+    {{-- Products --}}
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#productCollapse" aria-expanded="true" aria-controls="productCollapse">
+          <i class="fas fa-cubes"></i>
+          <span>Produk</span>
+        </a>
+        <div id="productCollapse" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Pilihan Produk:</h6>
+            <a class="collapse-item" href="{{route('product.index')}}">Produk</a>
+            <a class="collapse-item" href="{{route('product.create')}}">Tambah Produk</a>
+          </div>
+        </div>
+    </li>
+
+    {{-- Brands --}}
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#brandCollapse" aria-expanded="true" aria-controls="brandCollapse">
+          <i class="fas fa-table"></i>
+          <span>Brands</span>
+        </a>
+        <div id="brandCollapse" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Pilihan Brand:</h6>
+            <a class="collapse-item" href="{{route('brand.index')}}">Brand</a>
+            <a class="collapse-item" href="{{route('brand.create')}}">Tambah Brand</a>
+          </div>
+        </div>
+    </li>
+
+    {{-- Shipping --}}
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#shippingCollapse" aria-expanded="true" aria-controls="shippingCollapse">
+          <i class="fas fa-truck"></i>
+          <span>Pengiriman</span>
+        </a>
+        <div id="shippingCollapse" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Pilihan Pengiriman:</h6>
+            <a class="collapse-item" href="{{route('shipping.index')}}">Pengiriman</a>
+            <a class="collapse-item" href="{{route('shipping.create')}}">Tambah Pengiriman</a>
+          </div>
+        </div>
+    </li>
+
+    <!--Orders -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('order.index')}}">
+            <i class="fas fa-hammer fa-chart-area"></i>
+            <span>Orders</span>
+        </a>
+    </li>
+
+    <!-- Reviews -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{route('review.index')}}">
+            <i class="fas fa-comments"></i>
+            <span>Reviews</span></a>
+    </li>
+    
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+      Posts
+    </div>
+
+    <!-- Posts -->
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#postCollapse" aria-expanded="true" aria-controls="postCollapse">
+        <i class="fas fa-fw fa-folder"></i>
+        <span>Posts</span>
+      </a>
+      <div id="postCollapse" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Pilihan Posts:</h6>
+          <a class="collapse-item" href="{{route('post.index')}}">Posts</a>
+          <a class="collapse-item" href="{{route('post.create')}}">Tambah Posts</a>
+        </div>
+      </div>
+    </li>
+
+     <!-- Category -->
+     <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#postCategoryCollapse" aria-expanded="true" aria-controls="postCategoryCollapse">
+          <i class="fas fa-sitemap fa-folder"></i>
+          <span>Kategori</span>
+        </a>
+        <div id="postCategoryCollapse" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Pilihan Kategori:</h6>
+            <a class="collapse-item" href="{{route('post-category.index')}}">Kategori</a>
+            <a class="collapse-item" href="{{route('post-category.create')}}">Tambah Kategori </a>
+          </div>
+        </div>
+      </li>
+
+      <!-- Tags -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#tagCollapse" aria-expanded="true" aria-controls="tagCollapse">
+            <i class="fas fa-tags fa-folder"></i>
+            <span>Tags</span>
+        </a>
+        <div id="tagCollapse" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Pilihan Tags:</h6>
+            <a class="collapse-item" href="{{route('post-tag.index')}}">Tag</a>
+            <a class="collapse-item" href="{{route('post-tag.create')}}">Tambah Tags</a>
+            </div>
+        </div>
+    </li>
+
+      <!-- Comments -->
+      <li class="nav-item">
+        <a class="nav-link" href="{{route('comment.index')}}">
+            <i class="fas fa-comments fa-chart-area"></i>
+            <span>Komen</span>
+        </a>
+      </li>
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+     <!-- Heading -->
+    <div class="sidebar-heading">
+        Pengaturan Umum
+    </div>
+    <li class="nav-item">
+      <a class="nav-link" href="{{route('coupon.index')}}">
+          <i class="fas fa-table"></i>
+          <span>Kupon</span></a>
+    </li>
+     <!-- Users -->
+     <li class="nav-item">
+        <a class="nav-link" href="{{route('users.index')}}">
+            <i class="fas fa-users"></i>
+            <span>Users</span></a>
+    </li>
+     <!-- General settings -->
+     <li class="nav-item">
+        <a class="nav-link" href="{{route('settings')}}">
+            <i class="fas fa-cog"></i>
+            <span>Setting</span></a>
+    </li>
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
---
+++ resources/views/user/review/show.blade.php
@@ -0,0 +1,152 @@
+@extends('user.layouts.master')
+
+@section('title','Order Detail')
+
+@section('main-content')
+<div class="card">
+<h5 class="card-header">Order       <a href="{{route('order.pdf',$order->id)}}" class=" btn btn-sm btn-primary shadow-sm float-right"><i class="fas fa-download fa-sm text-white-50"></i> Generate PDF</a>
+  </h5>
+  <div class="card-body">
+    @if($order)
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+            <th>S.N.</th>
+            <th>No. Pemesanan</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jumlah</th>
+            <th>Biaya</th>
+            <th>Sub Total</th>
+            <th>Status</th>
+            <th>Aksi</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td>{{$order->id}}</td>
+            <td>{{$order->cart_id}}</td>
+            <td>{{$order->first_name}} {{$order->last_name}}</td>
+            <td>{{$order->email}}</td>
+            <td>{{$order->quantity}}</td>
+            <td>Rp. {{number_format($order->delivery_charge,2, ",", ".")}}</td>
+            <td>Rp. {{number_format($order->total_amount,2, ",", ".")}}</td>
+            <td>
+                @if($order->status=='new')
+                  <span class="badge badge-primary">{{$order->status}}</span>
+                @elseif($order->status=='process')
+                  <span class="badge badge-warning">{{$order->status}}</span>
+                @elseif($order->status=='delivered')
+                  <span class="badge badge-success">{{$order->status}}</span>
+                @else
+                  <span class="badge badge-danger">{{$order->status}}</span>
+                @endif
+            </td>
+            <td>
+                <a href="{{route('order.edit',$order->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="edit" data-placement="bottom"><i class="fas fa-edit"></i></a>
+                <form method="POST" action="{{route('order.destroy',[$order->id])}}">
+                  @csrf 
+                  @method('delete')
+                      <button class="btn btn-danger btn-sm dltBtn" data-id={{$order->id}} style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Delete"><i class="fas fa-trash-alt"></i></button>
+                </form>
+            </td>
+          
+        </tr>
+      </tbody>
+    </table>
+
+    <section class="confirmation_part section_padding">
+      <div class="order_boxes">
+        <div class="row">
+          <div class="col-lg-6 col-lx-4">
+            <div class="order-info">
+              <h4 class="text-center pb-4">INFORMASI PEMESANAN</h4>
+              <table class="table">
+                    <tr class="">
+                        <td>Nomor Pemesanan</td>
+                        <td> : {{$order->cart_id}}</td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Pemesanan</td>
+                        <td> : {{$order->created_at->diffForHumans()}}</td>
+                    </tr>
+                    <tr>
+                        <td>Jumlah</td>
+                        <td> : {{$order->quantity}}</td>
+                    </tr>
+                    <tr>
+                        <td>Status Pemesanan</td>
+                        <td> : {{$order->status}}</td>
+                    </tr>
+                    <tr>
+                        <td>Biaya Pengiriman</td>
+                        <td> : Rp. {{number_format($order->delivery_charge,2, ",", ".")}}</td>
+                    </tr>
+                    <tr>
+                        <td>Sub Total</td>
+                        <td> : Rp. {{number_format($order->total_amount,2, ",", ".")}}</td>
+                    </tr>
+                    <tr>
+                        <td>Metode Pembayaran</td>
+                        <td> : </td>
+                    </tr>
+                    <tr>
+                        <td>Status Pembayaran</td>
+                        <td> : </td>
+                    </tr>
+              </table>
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-lx-4">
+            <div class="shipping-info">
+              <h4 class="text-center pb-4">INFORMASI PENGIRIMAN</h4>
+              <table class="table">
+                    <tr class="">
+                        <td>Nama Lengkap</td>
+                        <td> : {{$order->first_name}} {{$order->last_name}}</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td> : {{$order->email}}</td>
+                    </tr>
+                    <tr>
+                        <td>Nomor Telepon</td>
+                        <td> : {{$order->phone}}</td>
+                    </tr>
+                    <tr>
+                        <td>Alamat</td>
+                        <td> : {{$order->address1}}, {{$order->address2}}</td>
+                    </tr>
+                    <tr>
+                        <td>Negara</td>
+                        <td> : {{$order->country}}</td>
+                    </tr>
+                    <tr>
+                        <td>Kode Post</td>
+                        <td> : {{$order->post_code}}</td>
+                    </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    @endif
+
+  </div>
+</div>
+@endsection
+
+@push('styles')
+<style>
+    .order-info,.shipping-info{
+        background:#ECECEC;
+        padding:20px;
+    }
+    .order-info h4,.shipping-info h4{
+        text-decoration: underline;
+    }
+
+</style>
+@endpush
