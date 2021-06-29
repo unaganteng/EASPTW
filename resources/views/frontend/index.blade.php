898b27c51518e7bd50d72c8b97313baf7a7b9cd7
unaganteng <rismasykes10@gmail.com>
2021-06-29 19:16:29 +0700

    update1

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -371,15 +371,15 @@
         <div class="row">
             <div class="col-12">
                 <div class="section-title">
-                    <h2>From Our Blog</h2>
+                    <!-- <h2>From Our Blog</h2> -->
                 </div>
             </div>
         </div>
-        <div class="row">
+        <!-- <div class="row">
             @if($posts)
                 @foreach($posts as $post)
                     <div class="col-lg-4 col-md-6 col-12">
-                        <!-- Start Single Blog  -->
+                        Start Single Blog 
                         <div class="shop-single-blog">
                             <img src="{{$post->photo}}" alt="{{$post->photo}}">
                             <div class="content">
@@ -388,12 +388,12 @@
                                 <a href="{{route('blog.detail',$post->slug)}}" class="more-btn">Continue Reading</a>
                             </div>
                         </div>
-                        <!-- End Single Blog  -->
+                        End Single Blog 
                     </div>
                 @endforeach
             @endif
             
-        </div>
+        </div> -->
     </div>
 </section>
 <!-- End Shop Blog  -->
